b23ab4f62ce8489d8f339808dc8ed149e0ae8476
EdwinDev <ecro98.98@gmail.com>
2025-01-12 13:14:06 -0500

    RepeatWords

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {maps} from "./double"
 import { invert } from "./invert";
 import { getGrade } from "./validateGrade";
 import { summation } from "./sumAllNumers";
+import { repeatStr } from "./repeatWord";
 
 
 console.log(maps([1, 2, 3]));
@@ -9,5 +10,6 @@ console.log(invert([1,2,3,4,5]));
 
 console.log(getGrade(44, 55, 52));
 console.log(summation(8));
+console.log(repeatStr(2, "*"));
 
 
